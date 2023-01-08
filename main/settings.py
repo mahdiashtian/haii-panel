eb1c7807482e6b975cdc86183aae7994a73ea142
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-08 12:52:30 +0330

    Adding a membership request filter and changing the database

--- main/settings.py
+++ main/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
-    # 'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +75,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME_DB'),
+        'USER': config('USER_DB'),
+        'PASSWORD': config('PASSWORD_DB'),
+        'HOST': config('HOST_DB'),
+        'PORT': config('PORT_DB'),
     }
 }
 
--- teams/models.py
+++ teams/models.py
@@ -1,6 +1,7 @@
 import uuid
 
-from django.core.validators import RegexValidator
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
 from utils.utils import upload_image_path
@@ -17,6 +18,7 @@ class Team(ID):
     # activity :Inversely related to 'Team' from 'Activity'
     # team_user :Inversely related to 'Team' from 'TeamUser'
     # membership_request :Inversely related to 'Team' from 'MembershipRequest'
+    # member_recruitment_filter :Inversely related to 'Team' from 'MemberRecruitmentFilter'
 
     name = models.CharField(max_length=256, verbose_name="نام تیم")
     description = models.TextField(verbose_name="توضیحات")
@@ -33,6 +35,8 @@ class Team(ID):
 
 class Activity(ID):
     # children :Inversely related to 'Activity' from 'Activity'
+    # membership_request :Inversely related to 'Activity' from 'MembershipRequest'
+    # member_recruitment_filter :Inversely related to 'Activity' from 'MemberRecruitmentFilter'
 
     name = models.CharField(max_length=256, verbose_name='نام فعالیت')
     description = models.TextField(verbose_name='توضیحات فعالیت')
@@ -49,6 +53,37 @@ class Activity(ID):
         if self.parent:
             self.team = None
         super().save(*args, **kwargs)
+
+    class Meta:
+        app_label = 'teams'
+
+
+class MemberRecruitmentFilter(ID):
+    class GenderChoices(models.TextChoices):
+        MAN = 'M', ('مرد')
+        FEMALE = 'F', ('زن')
+
+    class MembershipChoices(models.TextChoices):
+        PERMANENT = 'PE', ('دائمی')
+        FREELANCER = 'FR', ('فریلنسر')
+        LEARNER = 'LE', ('کارآموز')
+
+    age = ArrayField(models.PositiveSmallIntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(50)]), verbose_name='سن', size=2)
+    experience = ArrayField(models.PositiveSmallIntegerField(validators=[
+        MinValueValidator(1),
+        MaxValueValidator(50)]), verbose_name='سابقه', size=2)
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices, verbose_name='جنسیت')
+    city = models.CharField(max_length=256, verbose_name='شهر')
+    membership_type = models.CharField(max_length=2, choices=MembershipChoices.choices, verbose_name='نوع عضویت')
+    team = models.OneToOneField("teams.Team", on_delete=models.CASCADE, related_name='member_recruitment_filter',
+                                verbose_name='تیم مربوطه')
+    activity = models.ManyToManyField("teams.Activity", related_name='member_recruitment_filter',
+                                      verbose_name='فعالیت مربوطه')
+
+    def __str__(self):
+        return self.id
 
     class Meta:
         app_label = 'teams'
@@ -84,6 +119,8 @@ class MembershipRequest(ID):
     cv = models.FileField(upload_to=upload_image_path, verbose_name='رزومه')
     status = models.BooleanField(default=False, verbose_name='رد/تایید شده')
     description = models.TextField(verbose_name='توضیحات')
+    activity = models.ManyToManyField("teams.Activity", related_name='membership_request',
+                                      verbose_name='بخش های فعالیت')
 
     def __str__(self):
         return self.full_name
