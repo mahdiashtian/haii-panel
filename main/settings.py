8cf58b94b0d9d28f6c930a2b33feaaa5387df58a
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-11 22:39:33 +0330

    Apply the settings of the rest_framework

--- main/settings.py
+++ main/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    
     'users',
     'teams',
     'foods',
@@ -138,3 +140,9 @@ URL_MEDIA = 'haii/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
