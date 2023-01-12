b12fadeeffc94a33f2db449983449e85b805ca29
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-12 12:19:14 +0330

    Create education serializer

--- users/serializers/information.py
+++ users/serializers/information.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from users.models import Skill
+from users.models import Skill, Education
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
+        fields = '__all__'
+
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
         fields = '__all__'
