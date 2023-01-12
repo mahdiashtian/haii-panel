81f4e593018175c7370184d0a7ecfefe8ff81b8d
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-12 13:52:36 +0330

    Create food and desire serializer

--- foods/serializer.py
+++ foods/serializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from foods.models import WeeklyMeal
+from foods.models import WeeklyMeal, FoodAndDesire
 
 
 class WeeklyMealSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeeklyMeal
         fields = '__all__'
+
+
+class FoodAndDesireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodAndDesire
+        fields = '__all__'
+
