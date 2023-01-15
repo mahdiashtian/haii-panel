e693f93fe01e7a45276b4d96243700695a793106
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-15 13:12:58 +0330

    Create payment food view

--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from foods.models import PaymentFood
+from foods.serializer import PaymentFoodSerializer
+from main.permissions import IsOwner, IsSuperUser
+
+
+class PaymentFoodRUAV(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsOwner,)
+    queryset = PaymentFood.objects.all()
+    serializer_class = PaymentFoodSerializer
+
+
+class PaymentFoodRUAVMe(PaymentFoodRUAV):
+    def get_object(self):
+        return self.queryset.get(user=self.request.user.id)
+
+
+class PaymentFoodLAV(generics.ListAPIView):
+    permission_classes = (IsSuperUser,)
+    queryset = PaymentFood.objects.all()
+    serializer_class = PaymentFoodSerializer
