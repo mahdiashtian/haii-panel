fec4fca23d1a2e88d640b93a14a9a4b23220e09b
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-17 14:46:48 +0330

    Add food and desire view

--- foods/views.py
+++ foods/views.py
@@ -1,8 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
+from rest_framework.viewsets import GenericViewSet
 
-from foods.models import PaymentFood
-from foods.serializer import PaymentFoodSerializer
+from foods.models import PaymentFood, FoodAndDesire
+from foods.serializer import PaymentFoodSerializer, FoodAndDesireSerializer
 from main.permissions import IsOwner, IsSuperUser
+
+
+class FoodAndDesireViewSet(mixins.CreateModelMixin,
+                           mixins.RetrieveModelMixin,
+                           mixins.UpdateModelMixin,
+                           mixins.ListModelMixin,
+                           GenericViewSet):
+    queryset = FoodAndDesire.objects.all()
+    serializer_class = FoodAndDesireSerializer
+    permission_classes = [IsSuperUser]
 
 
 class PaymentFoodRUAV(generics.RetrieveUpdateAPIView):
