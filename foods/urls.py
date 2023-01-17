5293664dd1a9c3471e22e933dface1549f0c0473
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-17 15:20:06 +0330

    Create weekly meal and weekly meal user url

--- foods/urls.py
+++ foods/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from foods.views import PaymentFoodLAV, PaymentFoodRUAV, PaymentFoodRUAVMe, FoodAndDesireViewSet
+from foods.views import PaymentFoodLAV, PaymentFoodRUAV, PaymentFoodRUAVMe, FoodAndDesireViewSet, WeeklyMealViewSet,WeeklyMealUserViewSet
 
 app_name = 'foods'
 
 router = DefaultRouter()
 router.register('food-and-desire', FoodAndDesireViewSet, basename='food-and-desire')
+router.register('weekly-meal', WeeklyMealViewSet, basename='weekly-meal')
+router.register('weekly-meal-user', WeeklyMealUserViewSet, basename='weekly-meal-user')
 
 urlpatterns = [
     path('payment-food/', PaymentFoodLAV.as_view(), name='payment-food-list'),
