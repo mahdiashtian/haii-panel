5265f1b169fed1cf3e9ca1f18d04a61d8c0cb430
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-17 14:47:00 +0330

    Add food and desire url

--- foods/urls.py
+++ foods/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from foods.views import PaymentFoodLAV, PaymentFoodRUAV, PaymentFoodRUAVMe
+from foods.views import PaymentFoodLAV, PaymentFoodRUAV, PaymentFoodRUAVMe, FoodAndDesireViewSet
 
 app_name = 'foods'
+
+router = DefaultRouter()
+router.register('food-and-desire', FoodAndDesireViewSet, basename='food-and-desire')
 
 urlpatterns = [
     path('payment-food/', PaymentFoodLAV.as_view(), name='payment-food-list'),
     path('payment-food/<uuid:pk>/', PaymentFoodRUAV.as_view(), name='payment-food-detail'),
     path('payment-food/me/', PaymentFoodRUAVMe.as_view(), name='payment-food-me'),
+
+    path('', include(router.urls)),
 ]
