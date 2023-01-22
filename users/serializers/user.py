4c575e513bc5a8f36a14d06e7a840e87a4cfbe30
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-22 18:10:42 +0330

    Add CheckDestinationAccountSerializer and SendCreditSerializer

--- users/serializers/user.py
+++ users/serializers/user.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from users.exception import UserDoesNotExist, CreditAmountMustBePositive, CreditNotEnough, SelfCredit
+
 User = get_user_model()
 
 
@@ -8,3 +10,34 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class CheckDestinationAccountSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+
+    def validate_username(self, value):
+        user = User.objects.filter(username=value).exists()
+        if not user:
+            raise UserDoesNotExist
+        return value
+
+
+class SendCreditSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    amount = serializers.IntegerField()
+
+    def validate_username(self, value):
+        user = User.objects.filter(username=value).exists()
+        if not user:
+            raise UserDoesNotExist
+        if self.context['request'].user.username == value:
+            raise SelfCredit
+        return value
+
+    def validate_amount(self, value):
+        user = self.context['request'].user
+        if value < 0:
+            raise CreditAmountMustBePositive
+        if user.credit < value:
+            raise CreditNotEnough
+        return value
