78d0ba89e375ce111709c817c9a395da7f3345ce
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-22 18:05:11 +0330

    Add CheckDestinationAccount and SendCredit

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
 from rest_framework_simplejwt.views import TokenVerifyView
-from user.views import TokenObtainPairView, TokenRefreshView
+
+from users.views import TokenObtainPairView, TokenRefreshView, CheckDestinationAccount, SendCredit
 
 app_name = 'users'
 
@@ -8,5 +9,7 @@ urlpatterns = [
     re_path(r"^auth/jwt/create/?", TokenObtainPairView.as_view(), name="jwt-create"),
     re_path(r"^auth/jwt/refresh/?", TokenRefreshView.as_view(), name="jwt-refresh"),
     re_path(r"^auth/jwt/verify/?", TokenVerifyView.as_view(), name="jwt-verify"),
+    re_path(r"^check-destination-account/?", CheckDestinationAccount.as_view(), name="check-destination-account"),
+    re_path(r"^send-credit/?", SendCredit.as_view(), name="send-credit"),
 
 ]
