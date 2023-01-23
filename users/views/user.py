522865afbf2b1250a586ad40db251254c4a5909b
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-01-23 21:02:44 +0330

    Update user.py

--- users/views/user.py
+++ users/views/user.py
@@ -5,6 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -29,17 +30,13 @@ class SendCredit(APIView):
         serializer.is_valid(raise_exception=True)
         data = serializer.data
         user = User.objects.get(username=data['username'])
-        user.credit += data['amount']
-        user.save()
         user_origin = request.user
-        user_origin.credit -= data['amount']
-        user_origin.save()
         TransactionHistory.objects.create(
             user_sender=user_origin,
             user_receiver=user,
             price=data['amount'],
             transaction_type='TT',
-            status=True,
+            status="AC",
         )
 
         return Response(
@@ -48,10 +45,16 @@ class SendCredit(APIView):
         )
 
 
+class IncreaseCreditCardNumberShow(APIView):
+    def get(self, request):
+        return Response({'card_number': settings.CARD_NUMBER, 'owner_name': settings.OWNER_NAME})
+
+
 class IncreaseCreditCardNumberViewSet(viewsets.ModelViewSet):
     queryset = TransactionHistory.objects.all()
     serializer_class = TransactionHistorySerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['user_receiver__username', 'description']
     filterset_fields = {
         'date': ['lt', 'gt'],
         'status': ['exact'],
@@ -59,24 +62,23 @@ class IncreaseCreditCardNumberViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if not self.request.user.is_superuser:
-            return self.queryset.filter(Q(user_receiver=self.request.user) | Q(user_sender=self.request.user))
+            return self.queryset.filter(Q(user_receiver=self.request.user))
         return self.queryset
 
     def perform_create(self, serializer):
-        serializer.save(user_receiver=self.request.user, transaction_type='CAV', status=False)
+        user = self.request.user
+        if user.is_superuser:
+            status = 'AC'
+        else:
+            status = 'WA'
+        serializer.save(user_receiver=self.request.user, status=status)
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         return response
 
-    def perform_update(self, serializer):
-        result = serializer.save(status=True)
-        user = result.user_receiver
-        user.credit += result.price
-        user.save()
-
     def get_permissions(self):
-        if self.action in ['list', 'retrieve', 'create','information']:
+        if self.action in ['list', 'retrieve', 'create', 'information']:
             permission_classes = [IsAuthenticated]
         else:
             permission_classes = [IsSuperUser]
@@ -84,12 +86,12 @@ class IncreaseCreditCardNumberViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         serializer_class = self.serializer_class
-        if self.request.method == 'PUT':
+        if self.action in ['update', 'partial_update']:
             serializer_class.Meta.read_only_fields = (
                 'user_sender', 'document', 'date', 'transaction_type', 'user_receiver', 'description', 'price')
-        elif self.request.method == 'POST':
+        elif self.action == 'create':
             serializer_class.Meta.read_only_fields = (
-                'user_sender', 'status', 'date', 'transaction_type', 'user_receiver')
+                'user_sender', 'status', 'date', 'user_receiver')
 
         return serializer_class
 
@@ -97,8 +99,8 @@ class IncreaseCreditCardNumberViewSet(viewsets.ModelViewSet):
     def information(self, request, *args, **kwargs):
         user = self.request.user
         queryset = self.get_queryset()
-        credit_in_the_month = queryset.filter(date__month__exact=1).aggregate(Sum('price'))
-        credit_blocked = queryset.filter(status=False).aggregate(Sum('price'))
+        credit_in_the_month = queryset.filter(date__month__exact=1, status="AC").aggregate(Sum('price'))
+        credit_blocked = queryset.filter(status="RE").aggregate(Sum('price'))
         count_all = queryset.aggregate(Sum('price'))
         if user.is_superuser:
             debt = User.objects.filter(credit__lte=0).aggregate(Sum('credit'))
@@ -111,8 +113,3 @@ class IncreaseCreditCardNumberViewSet(viewsets.ModelViewSet):
             'count_all': count_all,
 
         })
-
-
-class IncreaseCreditCardNumberShow(APIView):
-    def get(self, request):
-        return Response({'card_number': settings.CARD_NUMBER, 'owner_name': settings.OWNER_NAME})
